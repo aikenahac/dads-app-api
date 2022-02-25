7d11c313f971c5899990f7fcfd5d9a9715ddc5ee
Aiken Tine Ahac <ahac.aiken@gmail.com>
2022-02-25 19:12:59 +0100

    Populate family in /users/me

---
+++ src/extensions/users-permissions/strapi-server.js
@@ -0,0 +1,14 @@
+module.exports = ( plugin ) => {
+  plugin.controllers.user.me = async ( ctx ) => {
+    const user = ctx.state.user;
+
+    if (!user) {
+      return ctx.unauthorized();
+    }
+
+    // ctx.body = await sanitizeOutput(user, ctx);
+    ctx.body = await strapi.query("plugin::users-permissions.user").findOne({ where: { id: user.id }, populate: ['family']})
+  };
+
+  return plugin;
+}
